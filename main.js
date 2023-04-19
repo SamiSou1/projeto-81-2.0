291649fdcf90db05614af412e39daa8d2fc295c1
SamiSou1 <121313494+SamiSou1@users.noreply.github.com>
2023-04-19 17:41:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+canvas = document.getElementById("meuCanvas");
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle - "grey";
+ctx.linewidth -1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle - "red";
+ctx.linewidth -5;
+ctx.arc(300, 200, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle - "black";
+ctx.linewidth -5;
+ctx.arc(200, 200, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle - "blue";
+ctx.linewidth -5;
+ctx.arc(400, 200, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle - "blue";
+ctx.linewidth -5;
+ctx.arc(350, 250, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle - "blue";
+ctx.linewidth -5;
+ctx.arc(250, 250, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+
+
